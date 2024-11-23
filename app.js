b4806a60fe0e82a226570a4e41d36c4557726fd7
Markiyan Prisyazhnik <125309736+MarkiyanPr@users.noreply.github.com>
2024-11-23 23:09:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/blog", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("MongoDB Atlas connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 // Start server
 app.listen(PORT, () => {
